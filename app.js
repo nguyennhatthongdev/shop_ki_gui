a466fa4f1cbe299ee42b652b7de2dd3ebf60733e
Macbook Air M4 <macbookairm4@MacBook-Air-cua-Macbook.local>
2026-07-04 15:02:36 +0700

    Phục hồi đường dẫn direct lh3.googleusercontent sau khi folder đã được chia sẻ công khai

--- app.js
+++ app.js
@@ -157,8 +157,7 @@ const resolveImageUrl = (url) => {
   if (urlClean.includes('drive.google.com') || urlClean.includes('docs.google.com')) {
     const match = urlClean.match(/id=([a-zA-Z0-9-_]+)/) || urlClean.match(/\/d\/([a-zA-Z0-9-_]+)/);
     if (match && match[1]) {
-      const targetUrl = `https://drive.google.com/thumbnail?id=${match[1]}&sz=w1000`;
-      return `https://images1-focus-opensocial.googleusercontent.com/gadgets/proxy?container=focus&refresh=2592000&url=${encodeURIComponent(targetUrl)}`;
+      return `https://lh3.googleusercontent.com/d/${match[1]}`;
     }
   }
   
